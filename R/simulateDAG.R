54fa74829dd34b50ca846210a2e2b8a882952b4b
Louis Dijkstra <louisdijkstra@gmail.com>
2018-04-10 15:04:40 +0200

    Started with creating a DAG

---
+++ R/simulateDAG.R
@@ -0,0 +1,65 @@
+#' Spontaneous Reporting Data
+#'
+#' \strong{Valid Reports} Not any binary sequence is a valid report. Each report 
+#' should contain at least one drug and at least one event  (otherwise it would 
+#' never been sent to the spontaneous reporitng sytem). 
+#' While generating reports, we make sure that this is indeed the case. When one does not
+#' want to check the validity and wants to allow any binary sequence, one can set 
+#' \code{valid_reports} to \code{FALSE}. 
+#' 
+#' @param n_reports Number of reports (Default: 100)
+#' @param n_drugs Number of drugs (Default: 10)
+#' @param n_events Number of adverse drug events (Default: 10)
+#' @param alpha_drugs Alpha parameter for the drug marginal probabilities (Default: 1.0)
+#' @param beta_drugs Beta parameter for the drug marginal probabilities (Default: 5.0)
+#' @param alpha_events Alpha parameter for the event marginal probabilities (Default: 1.0)
+#' @param beta_events Beta parameter for the event marginal probabilities (Default: 5.0)
+#' @param n_correlated_pairs Number of drug-event pairs that will exhibit a nonzero correlation (Default: 2)
+#' @param valid_reports If \code{TRUE}, only valid reports (with at least one drug and at least one event) are accepted. (Default: \code{TRUE})
+#' @param max_iter Maximum number of iterations (Default: 1000) 
+#' @param seed The seed used by the RNG (Default: automatically set)
+#' @param verbose Verbosity (Default: \code{TRUE})
+#'
+#' @return \item{sr}{A data frame with the simulated reports. The columns are named \code{drug1}, \code{drug2} ..., \code{event1}, \code{event2}, ...}
+#'         \item{prob_drugs}{A list with marginal probabilities of the drugs}
+#'         \item{prob_events}{A list with marginal probabilities of the events}
+#'         \item{corrmat}{The correlation matrix}
+#'
+#' @seealso \code{\link{create2x2Tables}}, 
+#'          \code{\link{validReport}}              
+#' @export
+simulateDAG <- function(n_reports = 100, 
+                        n_drugs = 10, 
+                        n_events = 10,
+                        alpha_drugs = 1.0,
+                        beta_drugs = 5.0,
+                        alpha_events = 1.0,
+                        beta_events = 5.0,
+                        method = 'er', 
+                        exp_degree = 3, 
+                        theta_drugs = 2,
+                        n_correlated_pairs = 2,
+                        theta = 2, 
+                        valid_reports = TRUE, 
+                        max_iter = 1000, 
+                        seed = NULL,
+                        verbose = TRUE) { 
+  
+  # set the seed
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  
+  # initialize a list with the marginal probabilities for 
+  # the drugs and the events
+  prob_drugs <- rep(NA, n_drugs) 
+  prob_events <- rep(NA, n_events)
+  
+  DAG <- randDAG(n_drugs, exp_degree, method = method)  
+  
+  return(
+    list(
+      DAG = DAG 
+    )
+  )
+}
